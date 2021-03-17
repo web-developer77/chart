2998c762ca2bb7333ff4aabd4f4f8446a0112c01
alexandr <grisanovaleksandrdev@gmail.com>
2021-03-18 00:43:14 +1000

    buy button changed

--- components/BuyButton.js
+++ components/BuyButton.js
@@ -8,11 +8,18 @@ export default function BuyButton(){
   //  setInterval(() => console.log(state), 1000);
   //}, []);
 
-  const onClick = e => console.log(state.lastQuote.Value);
-  return(
-    <div className='buy-button'>
-      <span className='pair'>{state.pair.pair}</span>
-      <span className='price'>{state.lastQuote.Value}</span>
+  const onClick = e => {
+    window.alert(`${state.pair.pair}: ${state.lastQuote.Value}`);
+  };
+  //return(
+  //  <div className='buy-button'>
+  //    <span className='pair'>{state.pair.pair}</span>
+  //    <span className='price'>{state.lastQuote.Value}</span>
+  //  </div>
+  //);
+  return (
+    <div className='buy-button' onClick={onClick}>
+      <span className='pair'>Get Price</span>
     </div>
   );
 };
